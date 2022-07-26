63045ee1b7d96c76a3bf669734ef8f5d775d879c
Andriy Pustovit <andriy.pustovit@mail.ru>
2022-07-26 10:58:55 +0300

    finish task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const first = document.querySelector(".gallery");
+first.insertAdjacentHTML("beforeend", createImage(galleryItems));
+first.addEventListener("click", notOpenNewPage);
+
+function createImage(colors) {
+  return colors
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>
+`;
+    })
+    .join("");
+}
+
+function notOpenNewPage(evt) {
+  evt.preventDefault();
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: "250",
+});
